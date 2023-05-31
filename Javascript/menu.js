60646e6119d4c874193d3c32a4c01039056e50ce
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-06-01 00:52:07 +0200

    menu successful

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,10 +1,44 @@
-/*
-let icon = document.querySelector(".icon");
-icon.addEventListener("click", toggleNavbar);
 
-function toggleNavbar() {
-  let navigation = document.querySelector(".navigation");
-  navigation.classList.toggle("open");
+const burgerMenu = document.querySelector(".burgermenu");
+const navigation = document.querySelector(".navigation");
 
-  if (navbar.classList.toggle("open") === true) {}
-}*/
+burgerMenu.addEventListener("change", () => {
+  if (burgerMenu.checked) {
+    navigation.style.left = "0";
+    navigation.style.height = "85%";
+    navigation.style.bottom = "0";
+    navigation.style.top = "15%";
+    navigation.style.borderTopRightRadius = "30px";
+  } else {
+    navigation.style.left = "-300px";
+    navigation.style.height = "85%";
+    navigation.style.bottom = "0";
+    navigation.style.top = "15%";
+    navigation.style.borderTopRightRadius = "30px";
+  }
+});
+
+window.addEventListener("resize", () => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth >= 800) {
+    showNavigation();
+  } else {
+    hideNavigation();
+  }
+});
+
+function showNavigation() {
+  navigation.style.left = "0";
+  navigation.style.height = "100%";
+  navigation.style.top = "0";
+  navigation.style.borderTopRightRadius = "0";
+  burgerMenu.checked = true;
+}
+
+function hideNavigation() {
+  navigation.style.left = "-300px";
+  navigation.style.height = "100%";
+  navigation.style.top = "0";
+  navigation.style.borderTopRightRadius = "0";
+  burgerMenu.checked = false;
+}
