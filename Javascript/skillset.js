1d1f8fb8f1244fdf19ba13b035edabe950a74a2f
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-06-12 15:22:58 +0200

    updates

---
+++ Javascript/skillset.js
@@ -0,0 +1,9 @@
+// Get carousel content and buttons
+const background = document.querySelector(".skillset_skills");
+const carouselContainer = document.querySelector(".skillset_carousel");
+const carousel = document.querySelector(".carousel");
+const skill = document.querySelector(".skill");
+
+const nextButton = document.querySelector(".fa-chevron-right");
+const prevButton = document.querySelector(".fa-chevron-left");
+
